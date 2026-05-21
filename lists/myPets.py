1e210b92a1600905838528069092c1d083db9f42
Jason Weiland <jweiland8913@gmail.com>
2026-05-21 17:47:23 -0600

    lists

---
+++ lists/myPets.py
@@ -0,0 +1,8 @@
+my_pets = ["Sophie", "DeeDee", "Kora"]
+print("Enter a pet name:")
+name = input("# ")
+if name not in my_pets:
+    print("I do not have a pet named " + name)
+else:
+    print(name + " is my pet.")
+
